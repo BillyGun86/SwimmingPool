7400cdc1a358a912b2c86ada908e1ef1edeec292
BillyGun86 <billysam3@gmail.com>
2022-06-21 00:58:11 +0400

    init

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,17 @@
 public class Main {
+    public static void main(String[] args) {
+
+        int volume = 1200;
+        int fillingSpeed = 30; //30 litres per minute
+        int devastationSpeed = 10; //10 litres per minute
+        int current_volume = 0;
+        int count = 0;
+
+        while (current_volume != volume) {
+            current_volume = current_volume + fillingSpeed - devastationSpeed;
+            count++;
+        }
+
+        System.out.println(count);
+    }
 }
